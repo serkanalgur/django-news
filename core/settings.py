ba49ea896ac134eddb3fea80b02f061a7e6a93a0
Serkan Algur <info@wpadami.com>
2022-03-01 22:28:00 +0400

    Allow Heroku domain and Close Debug

--- core/settings.py
+++ core/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ll75ygk&g+i!v+k=xvdsdk^mnyxwu@fo#un-@rca)dt$jfu9m#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1']
+ALLOWED_HOSTS = ['localhost','127.0.0.1','django-news-test.herokuapp.com']
 
 
 # Application definition
