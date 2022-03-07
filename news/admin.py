96eb6e4682d90320089bc1029b7fe6d459a3de30
Serkan Algur <info@wpadami.com>
2022-03-08 02:25:15 +0400

    Changes

--- news/admin.py
+++ news/admin.py
@@ -4,25 +4,37 @@ from django.contrib import admin
 
 from .models import News, Category
 
+
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('cat_name','cat_slug')
+    list_display = ('cat_name', 'cat_slug')
     prepopulated_fields = {'cat_slug': ('cat_name',)}
     fieldsets = [
-        ('Name', {'fields': ['cat_name']}),
-        (None, {'fields': ['cat_slug']}),
+        ('Main Details', {'fields': ['cat_name','cat_slug']}),
     ]
 
+
 class NewsAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug','created_on','category')
+    list_display = ('title', 'slug', 'created_on', 'category')
     list_filter = ['created_on']
     prepopulated_fields = {'slug': ('title',)}
     search_fields = ['title', 'slug']
-    fieldsets = [
-        ('Main Info', {'fields': ['title','slug']}),
-        (None,{'fields': [('author','status','category')]}),
-        ('Content', {'fields': ['content']}),
-        ('Main Image', {'fields': ['cover']}),
-    ]
+    fieldsets = (
+        ('Main Info', {
+            'fields': ['title', 'slug', 'author', 'status', 'category'],
+            'classes': ('baton-tabs-init', 'baton-tab-fs-content', 'baton-tab-fs-image', ),
 
-admin.site.register(Category,CategoryAdmin)
-admin.site.register(News,NewsAdmin)
+        }),
+        ('Content', {
+            'fields': ('content', ),
+            'classes': ('tab-fs-content', ),
+
+        }),
+        ('Image', {
+            'fields': ('cover', ),
+            'classes': ('tab-fs-image', ),
+        }),
+    )
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(News, NewsAdmin)
--- news/models.py
+++ news/models.py
@@ -7,37 +7,55 @@ from slugify import slugify
 from ckeditor_uploader.fields import RichTextUploadingField
 from easy_thumbnails.fields import ThumbnailerImageField
 
+
 class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
     cat_name = models.CharField(max_length=200)
-    cat_slug = models.SlugField(max_length=200, default="", blank=True, unique=True)
+    cat_slug = models.SlugField(
+        max_length=200, default="", blank=True, unique=True)
 
     def save(self, *args, **kwargs):
         if not self.id:
-            self.cat_slug = slugify(self.cat_name)
+            self.cat_slug = slugify(self.cat_name, replacements=[
+                                    ['Ə', 'E'], ['ə', 'e']])
         super(Category, self).save(*args, **kwargs)
+
     def __str__(self):
         return self.cat_name
+
 
 STATUS = (
     (0, "Draft"),
     (1, "Publish"),
-	(2, "Future"),
+    (2, "Future"),
 )
 
+
 class News(models.Model):
+
+    class Meta:
+        verbose_name_plural = "News"
+
     title = models.CharField(max_length=200)
-    slug = models.SlugField(max_length=200, default="", blank=True, unique=True)
-    category = models.ForeignKey(Category, null=True, on_delete=models.SET_NULL)
+    slug = models.SlugField(max_length=200, default="",
+                            blank=True, unique=True)
+    category = models.ForeignKey(
+        Category, null=True, on_delete=models.SET_NULL)
     content = RichTextUploadingField()
-    author = models.ForeignKey(User, blank=True, on_delete= models.CASCADE,related_name='news')
-    updated_on = models.DateTimeField(auto_now= True)
+    author = models.ForeignKey(
+        User, blank=True, on_delete=models.CASCADE, related_name='news')
+    updated_on = models.DateTimeField(auto_now=True)
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
-    cover = ThumbnailerImageField(upload_to='images/',default="",blank=True)
+    cover = ThumbnailerImageField(upload_to='images/', default="", blank=True)
 
     def save(self, *args, **kwargs):
         if not self.id:
-            self.slug = slugify(self.title)
+            self.slug = slugify(self.title, replacements=[
+                                ['Ə', 'E'], ['ə', 'e']])
         super(News, self).save(*args, **kwargs)
 
     def was_published_recently(self):
@@ -46,5 +64,6 @@ class News(models.Model):
         was_published_recently.admin_order_field = '-created_on'
         was_published_recently.boolean = True
         was_published_recently.short_description = 'Published?'
+
     def __str__(self):
         return self.title
